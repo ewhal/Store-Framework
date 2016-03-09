ade881b08358a4d83100f0460a111ad526688792
Eliot Whalan <contact@pantsu.cat>
2016-03-09 20:33:38 +1000

    Add products table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `users` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id` int(256) NOT NULL AUTO_INCREMENT,
 	`user_name` varchar(60) NOT NULL,
 	`user_email` varchar(60) NOT NULL,
 	`user_password` varchar(60) NOT NULL,
@@ -9,4 +9,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `user_name` (`user_name`),
 	UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `products` (
+	`id` int(255) NOT NULL AUTO_INCREMENT,
+	`product_name` varchar(256) NOT NULL,
+	`dscription` varchar(512) NOT NULL,
+	`image` varchar(60) NOT NULL,
+	`price` float(11) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
